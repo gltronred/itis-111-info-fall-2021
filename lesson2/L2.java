620a09464bd2e9ddf64cb632f291fe32b1968185
Mansur Ziiatdinov <gltronred@gmail.com>
2021-09-09 10:55:31 +0300

    Ввод-вывод вещественных чисел

--- lesson2/L2.java
+++ lesson2/L2.java
@@ -6,10 +6,23 @@ import java.util.*;
 public class L2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = a*b*c;
-        System.out.println(d);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // int d = a*b*c;
+        // System.out.println(d);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println(a+b);
+        // Ввод вещественных чисел зависит
+        // от настроек локали:
+        //
+        // $ java L2
+        // 1,2 3,4
+        // 4.6
+        //
+        // $ LC_ALL=C java L2
+        // 1.2 3.4
+        // 4.6
     }
 }
