b6d3f283efbe49d83c884f7254379ee77034e45c
Mansur Ziiatdinov <gltronred@gmail.com>
2021-09-16 11:27:06 +0300

    Двойной факториал

--- lesson3/L3.java
+++ lesson3/L3.java
@@ -13,8 +13,20 @@ public class L3 {
         int n = sc.nextInt();
         int factorial = 1;
         for (int i=1; i<=n; i++) {
+            // i++;
+            // то же, что
+            // i = i + 1;
             factorial *= i;
+            // factorial *= i;
+            // то же, что
+            // factorial = factorial * i;
         }
         System.out.println(factorial);
+
+        // Вводится целое неотрицательное N
+        // Вывести N!!
+        // N!! =
+        // = 1*3*5*...*N, если N - нечетное
+        // = 2*4*6*...*N, если N - четное
     }
 }
