c5e8093cef693ca40c229ff3b1d63c8b1e0e87a8
Mansur Ziiatdinov <gltronred@gmail.com>
2021-09-16 11:15:09 +0300

    Задание: факториал

---
+++ lesson3/L3.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class L3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i=0; i<5; i++) {
+            System.out.println(i);
+        }
+
+        // Вводится целое неотрицательное N
+        // Вывести N! = 1*2*...*N (по определению 0! = 1)
+    }
+}
