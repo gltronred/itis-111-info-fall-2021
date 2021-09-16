742bc33fe74bb1a4d226614ccdd0beff36eabdb9
Mansur Ziiatdinov <gltronred@gmail.com>
2021-09-16 11:22:50 +0300

    Решение: факториал

--- lesson3/L3.java
+++ lesson3/L3.java
@@ -4,11 +4,17 @@ import java.util.*;
 public class L3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for (int i=0; i<5; i++) {
-            System.out.println(i);
-        }
+        // for (int i=0; i<5; i++) {
+        //     System.out.println(i);
+        // }
 
         // Вводится целое неотрицательное N
         // Вывести N! = 1*2*...*N (по определению 0! = 1)
+        int n = sc.nextInt();
+        int factorial = 1;
+        for (int i=1; i<=n; i++) {
+            factorial *= i;
+        }
+        System.out.println(factorial);
     }
 }
