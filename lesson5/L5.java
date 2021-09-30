5790a2ae0e2964f98d4013236f45a49c94a1f2a1
Mansur Ziiatdinov <gltronred@gmail.com>
2021-09-30 10:34:02 +0300

    Полином

---
+++ lesson5/L5.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class L5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Вводятся вещественное y
+        // Вводится неотрицательное целое n
+        // Вводятся коэффициенты полинома
+        // a_n a_{n-1} ... a_0
+        // Вывести значение P(y) полинома
+        // P(x) = a_n x^n + a_{n-1} x^{n-1} +
+        // + ... + a_1 x + a_0
+        // в точке y
+        //
+        // 2.0 2
+        // 1.5 -3.0 5.0
+        // 5.0
+        //
+        // (1.5 x^2 - 3 x + 5 для x = 2.0)
+    }
+}
