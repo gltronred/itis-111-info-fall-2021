369ea4b12464050284999a748d255e790dd5c75c
Mansur Ziiatdinov <gltronred@gmail.com>
2021-09-30 10:55:02 +0300

    Вычисление "в лоб"

--- lesson5/L5.java
+++ lesson5/L5.java
@@ -14,10 +14,22 @@ public class L5 {
         // + ... + a_1 x + a_0
         // в точке y
         //
-        // 2.0 2
-        // 1.5 -3.0 5.0
-        // 5.0
+        // >   2.0 2
+        // >   1.5 -3.0 5.0
+        // <   5.0
         //
         // (1.5 x^2 - 3 x + 5 для x = 2.0)
+        double y = sc.nextDouble();
+        int n = sc.nextInt();
+        double z = 0;
+        for (int k=n; k>=0; k--) {
+            double a = sc.nextDouble();
+            double p = a;
+            for (int i=0; i<k; i++) {
+                p *= y;
+            }
+            z += p;
+        }
+        System.out.println(z);
     }
 }
