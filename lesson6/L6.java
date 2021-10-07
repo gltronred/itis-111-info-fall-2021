7f908303e4ff37ad63602757e4cb247df143eb64
Mansur Ziiatdinov <gltronred@gmail.com>
2021-10-07 10:23:29 +0300

    Массивы

---
+++ lesson6/L6.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class L6 {
+    public static void main(String args[]) {
+        int[] a = new int[6];
+        for (int i=0; i<6; i++) {
+            a[i] = i;
+        }
+
+        for (int i=0; i<6; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
