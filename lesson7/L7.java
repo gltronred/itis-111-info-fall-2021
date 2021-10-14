0fc2c03d53b4c13638998627bed1cdf38c7e9494
Mansur Ziiatdinov <gltronred@gmail.com>
2021-10-14 14:57:44 +0300

    По диагоналям

--- lesson7/L7.java
+++ lesson7/L7.java
@@ -65,11 +65,7 @@ public class L7 {
         int[][] a = new int[n][n];
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                if (i % 2 == j % 2) {
-                    a[i][j] = 1;
-                } else {
-                    a[i][j] = 0;
-                }
+                a[i][j] = Math.abs(i-j);
             }
         }
         for (int i=0; i<n; i++) {
