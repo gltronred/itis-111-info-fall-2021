45529554848d9a309c235f2ac1fefe212bb957f0
Mansur Ziiatdinov <gltronred@gmail.com>
2021-10-14 10:21:50 +0300

    Двумерные массивы

---
+++ lesson7/L7.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class L7 {
+    public static void main(String args[]) {
+        int n = 4;
+        int[][] a = new int[n][];
+        for (int i=0; i<n; i++) {
+            a[i] = new int[2*i+1];
+            for (int j=0; j<2*i+1; j++) {
+                a[i][j] = i+j;
+            }
+        }
+
+        for (int i=0; i<n; i++) {
+            System.out.println(Arrays.toString(a[i]));
+        }
+
+
+    }
+}
