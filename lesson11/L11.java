cec33186aa621d4e62a75e6620ad78e991432b85
Mansur Ziiatdinov <gltronred@gmail.com>
2021-10-28 14:25:07 +0300

    Дополнение условия

--- lesson11/L11.java
+++ lesson11/L11.java
@@ -12,5 +12,14 @@ public class L11 {
         int[] r = add(a,b);
         // 10, 11, 10, 5, 6
         System.out.println(Arrays.toString(r));
+        // a и b должны остаться теми же
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+        // дальше тоже должно работать
+        int[] c = {9,9,7,1};
+        int[] p = add(a,c); // 10,11,10,5
+        int[] q = add(c,b); // 18,18,14,2
+        System.out.println(Arrays.toString(p));
+        System.out.println(Arrays.toString(q));
     }
 }
