c1c4a5af680176fecbcdd0e1cc26b1cc262d7a7f
Mansur Ziiatdinov <gltronred@gmail.com>
2021-11-18 11:12:49 +0300

    Более сложная лампочка

--- lesson13/L13.java
+++ lesson13/L13.java
@@ -15,33 +15,43 @@ class Counter {
 }
 
 class Lamp {
-    public void turnOn() {}
-    public void turnOff(){}
-    public boolean isOn(){ return false; }
+    boolean on;
+    // если включили не в 220 вольт,
+    // лампочка сломается и больше
+    // не будет включаться
+    public void turnOn(int voltage) {
+        on = true;
+    }
+    public void turnOff(){ on = false; }
+    public boolean isOn(){ return on; }
 }
 
 public class L13 {
-    public static Counter inc(Counter c) {
-        c.increment();
-        return c;
-    }
+    // public static Counter inc(Counter c) {
+    //     c.increment();
+    //     return c;
+    // }
     public static void main(String[] args) {
-        int[] x = new int[5];
-        Counter c1 = new Counter();
-        c1.increment();
-        System.out.println(c1.getCount());
-        Counter c2 = new Counter();
-        c1.increment();
-        System.out.println(c1.getCount());
-        System.out.println(c2.getCount());
-        System.out.println(inc(c1).getCount());
-        System.out.println(c1);
-        System.out.println(inc(c1));
+        // int[] x = new int[5];
+        // Counter c1 = new Counter();
+        // c1.increment();
+        // System.out.println(c1.getCount());
+        // Counter c2 = new Counter();
+        // c1.increment();
+        // System.out.println(c1.getCount());
+        // System.out.println(c2.getCount());
+        // System.out.println(inc(c1).getCount());
+        // System.out.println(c1);
+        // System.out.println(inc(c1));
 
         Lamp lamp = new Lamp();
-        lamp.turnOn();
+        lamp.turnOn(220); // включается
         System.out.println(lamp.isOn());
-        lamp.turnOff();
+        lamp.turnOff();   // выключается
+        System.out.println(lamp.isOn());
+        lamp.turnOn(380); // ломается
+        System.out.println(lamp.isOn());
+        lamp.turnOn(220); // не включается - сломана
         System.out.println(lamp.isOn());
     }
 }
