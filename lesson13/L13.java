965b5b1edcd1d7d82511a9599d2736446cfbe968
Mansur Ziiatdinov <gltronred@gmail.com>
2021-11-18 10:52:03 +0300

    toString

--- lesson13/L13.java
+++ lesson13/L13.java
@@ -9,10 +9,18 @@ class Counter {
     public int getCount() {
         return counter;
     }
+    public String toString() {
+        return "Counter:" + counter;
+    }
 }
 
 public class L13 {
+    public static Counter inc(Counter c) {
+        c.increment();
+        return c;
+    }
     public static void main(String[] args) {
+        int[] x = new int[5];
         Counter c1 = new Counter();
         c1.increment();
         System.out.println(c1.getCount());
@@ -20,6 +28,8 @@ public class L13 {
         c1.increment();
         System.out.println(c1.getCount());
         System.out.println(c2.getCount());
-
+        System.out.println(inc(c1).getCount());
+        System.out.println(c1);
+        System.out.println(inc(c1));
     }
 }
