b1a8afd9ae984764966ccba85b897a437e3471f9
Mansur Ziiatdinov <gltronred@gmail.com>
2021-12-09 15:25:04 +0300

    Static final

--- lesson15/L15.java
+++ lesson15/L15.java
@@ -3,9 +3,11 @@ import java.util.*;
 
 import ru.kfu.*;
 import com.company.Carry;
+import com.other.Other;
 
 public class L15 {
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
         Teacher teacher = new Teacher();
         System.out.println(teacher);
         Carry carry = new Carry();
@@ -25,5 +27,16 @@ public class L15 {
         // ошибка компиляции!
         // System.out.println(student.isPresent());
         carry.selfTest();
+
+        Other o1 = new Other(1);
+        Other o2 = new Other(2);
+        System.out.println(o1.getId());
+        System.out.println(o2.getId());
+
+        o1.increase();
+        o2.increase();
+        System.out.println(o1.getCount());
+        System.out.println(o2.getCount());
+        System.out.println(Other.getCount());
     }
 }
--- lesson15/com/other/Other.java
+++ lesson15/com/other/Other.java
@@ -2,7 +2,18 @@ package com.other;
 
 import java.util.*;
 
+// class Another extends Other {
+//     public int getId() { return 132; }
+// }
+
 public class Other {
-    public Other() {}
+    private final int id;
+    public Other(int id) { this.id = id; }
+    public final int getId() { return id; }
+
     public static double getSomething() { return Math.PI; }
+
+    private static int count;
+    public static int getCount() { return count; }
+    public void increase() { count++; }
 }
